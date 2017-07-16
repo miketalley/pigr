82ed43613e89eaee1e2a881e51f9e8df724464b2
Mike Talley <miketalley@hotmail.com>
2017-07-16 15:57:44 -0400

    finished generateCronCommand and writeNewCronTask

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+// Writes new cron task to /etc/crontab
+// Format:
+// # pigr task <taskId> - <taskName>
+// minute hour dayOfMonth month dayOfWeek user command
+// # end task <taskId>
+
 const { exec } = require('child_process');
 const fs = require('fs');
 
 const CRONTAB_FILE_LOCATION = '/etc/crontab';
 const CRONTAB_HEADER_START = '# pigr task ';
 const CRONTAB_FOOTER_START = '# end task ';
+const LINE_BREAK = '\r\n';
 
 function generateCronCommand(opts) {
-
+  return
+    opts.minute +
+    ' ' +
+    opts.hour +
+    ' ' +
+    opts.dayOfMonth +
+    ' ' +
+    opts.month +
+    ' ' +
+    opts.dayOfWeek +
+    ' ' +
+    opts.user +
+    ' ' +
+    opts.command +
+    ';'
 }
 
 function writeNewCronTask(content) {
-
+  fs.appendFileSync(CRONTAB_FILE_LOCATION, content);
 }
 
 function taskExists(id) {
@@ -42,18 +63,9 @@ function generateTaskContent(opts) {
   const footer = generateFooter(id);
   const command = generateCronCommand(cronCommandOptions);
 
-  return {
-    header,
-    footer,
-    command
-  };
+  return LINE_BREAK + header + LINE_BREAK + command + LINE_BREAK + footer + LINE_BREAK;
 }
 
-// Writes new cron task to /etc/crontab
-// Format:
-// # pigr task <taskId> - <taskName>
-// minute hour dayOfMonth month dayOfWeek user command
-// # end task <taskId>
 function addCronTask(opts) {
   const id = opts.id;
 
